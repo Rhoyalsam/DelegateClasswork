89c0933107bc8a0cbe21e4341583b7eb9d5226d8
Support <Support@WEMA-TRAINING-S>
2022-08-16 10:23:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DelegateClasswork
+{
+
+    /// <summary>
+    /// Main program
+    /// </summary>
+    public class Program
+    {
+        public delegate bool Predicate(XClass a);       //predicate
+
+        public bool WhereOne(Predicate b)       //method that takes in the delegate
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            var repoUser = new Program();
+            var x = new XClass();
+            var possibleUsername = x.FirstName + "." + x.LastName;
+            var dUserCount = repoUser.WhereOne(a => a.UserName == possibleUsername);
+        }
+
+    }
+    /// <summary>
+    /// Public class with the properties
+    /// </summary>
+    public class XClass
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string UserName { get; set; }
+    }
+}
